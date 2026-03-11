bb6f2591651bea56f12ca22029fecc87c736c8ad
Hontvári Attila <attila@hontvari.net>
2026-03-11 20:22:04 +0100

    scheduler helyesség tesztek elkezdve

---
+++ scheduler-test/rifo_test_empty_queue.py
@@ -0,0 +1,7 @@
+from scapy.all import *
+import time
+
+for i in range(0, 10):
+    p = IP(dst="10.0.0.1") / UDP(sport = 12345, dport=50005) / ("Hello world!" + str(i))
+    send(p)
+    time.sleep(1)
