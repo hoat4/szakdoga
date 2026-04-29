07280601c814c00567b5410cf8292418fe098521
Hontvári Attila <attila@hontvari.net>
2026-04-30 01:12:07 +0200

    komment javítás

--- measurement/test_throughput_above_link_capacity.py
+++ measurement/test_throughput_above_link_capacity.py
@@ -5,7 +5,7 @@ resetInterface()
 profiles = [
     makeASTFTemplate(50005, 10_000, 40), # 40 * 10KB = 4MBps = 3,2Mbps
     makeASTFTemplate(50007, 100_000, 20), # 20 * 100KB = 20MBps = 16Mbps
-    makeASTFTemplate(50009, 1_000_000, 12), # 12MBps = 96Mbps
+    makeASTFTemplate(50009, 1_000_000, 12), # 12 * 1MBps = 96Mbps
 ]
 
 # tehát összesen 1152 Mbps, míg a bottleneck 1Gbps
