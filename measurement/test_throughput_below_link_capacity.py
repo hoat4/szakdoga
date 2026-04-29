429872c907ed0c87efded220ac6cbb52cdbde231
Hontvári Attila <attila@hontvari.net>
2026-04-29 14:29:18 +0200

    több flow legyen, hogy ne jobban kiátlagolódjanak a completiontime mérési hibák

--- measurement/test_throughput_below_link_capacity.py
+++ measurement/test_throughput_below_link_capacity.py
@@ -1,13 +1,15 @@
 from traffic_common import *
 
-# 40*100KB-ot viszünk át, azaz 32Mbps, ami kisebb mint a link sebessége (100Mbps).
+# 8*500KB-ot viszünk át, azaz 32Mbps, ami kisebb mint a link sebessége (100Mbps).
 # dropnak 0 közelinek kéne lennie, throughputnak mindegyik esetben kb. ugyanannyinak.
 # SRTF most eléggé elrontja flow completion time-ot, főleg PIFO-nál.
+
+# eredetileg 40*100KB volt, de úgy túl zavarosak voltak a flow completion time adatok
 
 resetInterface()
 
 #profile = makeProfile(50011, 10000000, 10)
-templates = [makeASTFTemplate(50009, 100_000, 4)]
+templates = [makeASTFTemplate(50010, 500_000, 8)]
 
 runMeasurement("bfifo", None, templates)
 #runMeasurement("rifo", None) ezeket is lehet nézni, de ugyanaz lenne mint SJN-nek, mert csak egyetlen flow hossz van
